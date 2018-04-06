829da4ace1290278adb9ad7a669b37b1a4d166ea
mnap <learn@10g.pl>
2018-04-06 22:35:12 +0200

    Add Contact component

---
+++ components/Contact.js
@@ -0,0 +1,22 @@
+var Contact = React.createClass({
+    propTypes: {
+        item: React.PropTypes.object.isRequired
+    },
+    render: function() {
+        return (
+            React.createElement('div', {className: 'contactItem'}
+                React.createElement('img', {
+                    className: 'contactImage',
+                    src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
+                }),
+                React.createElement('p', {className: 'contactLabel'},
+                    'First name: ' + this.props.item.firstName),
+                React.createElement('p', {className: 'contactLabel'},
+                    'Last name: ' + this.props.item.lastName),
+                React.createElement('a', {className: 'contactEmail',
+                    href: 'mailto:' + this.props.item.email},
+                        this.props.item.email)
+            )
+        );
+    }
+});
