f0bc251460219916dbb6204ed1b6f01596eafb35
mnap <learn@10g.pl>
2018-04-08 21:42:12 +0200

    Update scripts to JSX; add babel-standalone

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -4,24 +4,17 @@ var ContactForm = React.createClass({
     },
     render: function () {
         return (
-            React.createElement('form', {className: 'contactForm'},
-                React.createElement('input', {
-                    type: 'text',
-                    placeholder: 'First name',
-                    value: this.props.contact.firstName
-                }),
-                React.createElement('input', {
-                    type: 'text',
-                    placeholder: 'Last name',
-                    value: this.props.contact.lastName
-                }),
-                React.createElement('input', {
-                    type: 'email',
-                    placeholder: 'Email',
-                    value: this.props.contact.email
-                }),
-                React.createElement('button', {type: 'submit'}, 'Add contact')
-            )
+            <form className='contactForm'>
+                <input type='text' placeholder='First name'
+                    value={this.props.contact.firstName} />
+                <input type='text' placeholder='Last name'
+                    value={this.props.contact.lastName} />
+                <input type='email' placeholder='Email'
+                    value={this.props.contact.email} />
+                <button type='submit'>
+                    Add contact
+                </button>
+            </form>
         );
     }
 });
